d72a69a7ce3bce98371fd87d06cc9d5105ee144d
aaron2422 <aaron.fu524@gmail.com>
2019-02-13 13:39:30 +0800

    Init project

---
+++ src/main/java/com/training/aaron/concurrencytraining/ConcurrencyTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.training.aaron.concurrencytraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyTrainingApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/training/aaron/concurrencytraining/ConcurrencyTrainingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.training.aaron.concurrencytraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
